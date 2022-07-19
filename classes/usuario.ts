03f35996120d0cf773a2aae87989789469b082a1
FelipeParedes <felipeparedes238@gmail.com>
2022-07-19 14:27:08 -0500

    mensajes privados con error

--- classes/usuario.ts
+++ classes/usuario.ts
@@ -2,10 +2,12 @@ export class Usuario {
     public id: string;
     public nombre: string;
     public sala: string;
+    public color: string;
     constructor(id: string){
         this.id = id;
         this.nombre = 'sin - nombre';
         this.sala = 'sin - sala';
+        this.color = 'black';
 
     }
 }
